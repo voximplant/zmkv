e2100c873bd72eae1bebc5a46e6682eda700f68f
root <asyvrachev@zingaya.com>
2015-11-20 07:20:31 +0000

    Correct find.cmake names

--- zmkv/cmake/FindEbml.cmake
+++
@@ -1,7 +0,0 @@
-include(${CMAKE_MODULE_PATH}/utils.cmake)
-
-find_component(EBML  libebml  ebml  ebml/EbmlVersion.h)
-
-message("EBML_LIBRARIES = ${EBML_LIBRARIES}")
-message("EBML_INCLUDE_DIRS = ${EBML_INCLUDE_DIRS}")
-message("EBML_VERSION = ${EBML_VERSION}")
--- zmkv/cmake/FindMatroska.cmake
+++
@@ -1,7 +0,0 @@
-include(${CMAKE_MODULE_PATH}/utils.cmake)
-
-find_component(MATROSKA libmatroska  matroska  matroska/KaxVersion.h)
-
-message("MATROSKA_LIBRARIES = ${MATROSKA_LIBRARIES}")
-message("MATROSKA_INCLUDE_DIRS = ${MATROSKA_INCLUDE_DIRS}")
-message("MATROSKA_VERSION = ${MATROSKA_VERSION}")
